b9b61104525edae8bcb0a3f6bd51d9020a930935
OgutuJoseph <onyango.joseph@yahoo.com>
2023-04-13 14:32:38 +0300

    Creating a test using mocha

---
+++ test/task.js
@@ -0,0 +1,20 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+
+// Assertion style
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Tasks API', () => {
+    /** Test the GET route */
+    
+    /** Test the GET By ID route */
+    
+    /** Test the POST route */
+    
+    /** Test the PUT route */
+    
+    /** Test the PATCH route */
+})
